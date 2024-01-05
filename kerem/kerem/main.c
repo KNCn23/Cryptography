a7d50e25ffed1f1c342020f75118e2affc4eda80
Kaan Olcay <akcolcay@hotmail.com>
2024-01-05 20:40:53 +0300

    bew

---
+++ kerem/kerem/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int dizi[5];
+    int i;
+    
+    printf("5 adet deger giriniz:");
+    
+    
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&dizi[i]);
+        
+    }
+    
+    printf("\n Girilen degerler sunlardir. ");
+    
+    for(i=0;i<5;i++)
+    {
+        printf("\n %d \n",dizi[i]);
+        
+    }
+    
+    return 0;
+}
